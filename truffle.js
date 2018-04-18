e21e2561e4e5b12c6ddbb7cc4938dd7e6cf19a0e
zhangying888 <zymontask@gmail.com>
2018-04-18 18:09:11 +0800

    modify some config

--- truffle.js
+++ truffle.js
@@ -3,12 +3,19 @@ module.exports = {
     'dev.fifs': {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gasPrice: 0x2e90edd000, //200GWei
+      gas: 1000000//,
+      //from: ""      
     },
     'dev.auction': {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gasPrice: 0x2e90edd000, //200GWei
+      //gasPrice: 0x2e90,
+      gas: 4700000,
+      from: ""
     }
   }
 };
